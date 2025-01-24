2a13e1b0a79ebf2315fa6d56b20ba2dfd30d2111
Mart Broekkamp <martbr90@gmail.com>
2025-01-24 23:11:01 +0100

    0.3.1

--- ACardGame/Main.cs
+++ ACardGame/Main.cs
@@ -29,7 +29,7 @@ namespace ACardGame
         public static int ScreenWidth;
         public static int ScreenHeight;
 
-        public const string GameVersion = "0.3";
+        public const string GameVersion = "0.3.1";
         public const bool DebugLogEnabled = true;
 
         public Main()
